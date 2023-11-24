1f04f73a3537e0d870f04071eb22d66c8062f6bd
Hoang Duc Duong <comehere.hdd@gmail.com>
2023-11-24 16:37:24 +0700

    file am thanh

--- Dictionary/src/main/java/frontend/SearchController.java
+++ Dictionary/src/main/java/frontend/SearchController.java
@@ -63,7 +63,10 @@ public class SearchController implements Initializable {
         Word tmp = DictionaryCommandline.dictionaryLookup(word);
         String text = tmp.getWordSpelling() + "\n" + tmp.getWordExplain();
         defTextArea.setText(text);
-        recentSearch.add(word);
+        if(Dictionary.recentWord.size() == 10) Dictionary.recentWord.remove(0);
+        if(Dictionary.recentWord.contains(word)) Dictionary.recentWord.remove(word);
+        Dictionary.recentWord.add(word);
+        recentSearch.addAll(Dictionary.recentWord);
 
         defTextArea.setVisible(true);
         saveBtn.setVisible(false);
