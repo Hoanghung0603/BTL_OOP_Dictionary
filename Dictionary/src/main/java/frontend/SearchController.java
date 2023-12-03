025df3a9c545a06d649031301e37651a85d40654
Hoanghung0603 <hoangduyhung021@gmail.com>
2023-12-03 21:26:31 +0700

    fix sth

--- Dictionary/src/main/java/frontend/SearchController.java
+++ Dictionary/src/main/java/frontend/SearchController.java
@@ -55,7 +55,6 @@ public class SearchController implements Initializable {
     // hàm này để dựa vào word, setText của correct word thành từ auto correct
     private void setCorrectWord(String word) {
         String word_da_sua = "hello";  // đang mặc định từ auto correct là hello
-
         correctWord.setText(word_da_sua);
     }
     @FXML
